49c6b31fc1392ee80de0d65ac6aade9f51d05ec2
Kay Strobach <github@kay-strobach.de>
2013-11-19 23:13:25 +0100

    [FEATURE] Add XLIFF Writer ... ;D

--- Classes/KayStrobach/DevelopmentTools/Controller/TranslationLabelController.php
+++ Classes/KayStrobach/DevelopmentTools/Controller/TranslationLabelController.php
@@ -1,11 +1,14 @@
 <?php
 namespace KayStrobach\DevelopmentTools\Controller;
 
+use KayStrobach\DevelopmentTools\Domain\Model\TranslationLabel;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Command\HelpCommandController;
 use TYPO3\Flow\Exception;
 use TYPO3\Flow\Cli\Command;
 use TYPO3\Flow\Cli\CommandManager;
+use TYPO3\Flow\Error\Message;
+
 
 /**
  * Der Startcontroller
@@ -25,6 +28,51 @@ class TranslationLabelController extends \TYPO3\Flow\Mvc\Controller\ActionContro
 
 	public function clearAllAction() {
 		$this->translationLabelRepository->removeAll();
+		$this->redirect('index');
+	}
+
+	/**
+	 * @param TranslationLabel $translationLabel
+	 */
+	public function addToXliffAction(TranslationLabel $translationLabel) {
+		$filename = 'resource://' . $translationLabel->getPackageKey() . '/Private/Translations/en/' . $translationLabel->getSourceName() . '.xlf';
+
+		if(file_exists($filename)) {
+			$doc = new \DOMDocument('1.0');
+			$doc->preserveWhiteSpace = false;
+			$doc->load($filename, LIBXML_COMPACT);
+			$doc->formatOutput = true;
+
+			if($doc->getElementById($translationLabel->getLabelId()) !== NULL) {
+				$this->addFlashMessage('This id %1s is already present in the Translation file', '', Message::SEVERITY_NOTICE, array($translationLabel->getLabelId()));
+				$this->redirect('index');
+				return;
+			}
+			$bodyElement = $doc->getElementsByTagName('body')->item(0);
+
+			$source = $doc->createElement('source', $translationLabel->getLabel());
+
+			$transUnit = $doc->createElement('trans-unit');
+			$transUnit->setAttribute('id', $translationLabel->getLabelId());
+			$transUnit->setIdAttribute('id', TRUE);
+			$transUnit->setAttribute('xml:space', 'preserve');
+			$transUnit->appendChild($source);
+
+			$bodyElement->appendChild($transUnit);
+
+			if(is_writable($filename)) {
+				$buffer = $doc->saveXML();
+				if(file_put_contents($filename, $buffer)) {
+					$this->addFlashMessage('File %1s saved', '', Message::SEVERITY_OK, array($filename));
+				} else {
+					$this->addFlashMessage('The xlf file %1s is not writeable for me', '', Message::SEVERITY_ERROR, array($filename));
+				}
+			} else {
+				$this->addFlashMessage('The xlf file %1s is not writeable for me', '', Message::SEVERITY_ERROR, array($filename));
+			}
+		} else {
+			$this->addFlashMessage('Can´ find file %1s', '', Message::SEVERITY_ERROR, array($filename));
+		}
 		$this->redirect('index');
 	}
 }
